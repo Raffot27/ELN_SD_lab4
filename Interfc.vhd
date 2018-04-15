511192b11df7ab5e78d7af48f4af0ffa705e52e7
CrazyBJT96 <38176829+CrazyBJT96@users.noreply.github.com>
2018-04-15 16:59:11 +0200

    Interfc_16_(Q+1)

---
+++ Interfc.vhd
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity Interfc is  
+port(KEY, SW : in std_logic_vector(1 downto 0); 
+	  HEX0, HEX1, HEX2, HEX3 : out std_logic_vector(6 downto 0)
+	  );
+	 end Interfc;
+architecture Behav of Interfc is
+
+component counter_16_sincrono 
+port(Enable, CLK, Clear: in std_logic; 
+	 Segmenti0, Segmenti1, Segmenti2, Segmenti3 : out std_logic_vector (6 downto 0));
+end component;
+
+begin 
+Counter : counter_16_sincrono port map (SW(1), KEY(0), SW(0), HEX0, HEX1, HEX2, HEX3 );
+end Behav;
